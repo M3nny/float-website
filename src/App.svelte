aa58ae77943f9db9f7a5ccd3eedaceae5746dde7
M3nny <lucamenegazzo3@gmail.com>
2021-12-27 13:00:22 +0100

    aaa

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,97 @@
 <script>
-	let name = 'world';
+	let bg = document.getElementById("bg");
+	let moon = document.getElementById("moon");
+	let mountain = document.getElementById("mountain");
+	let road = document.getElementById("road");
+	let text = document.getElementById("text");
+
+	window.addEventListener('scroll', function(){
+		var value = window.scrollY;
+
+		bg.style.top = value * 0.5 + 'px';
+		moon.style.left = -value * 0.5 + 'px';
+		mountain.style.top = -value * 0.15 + 'px';
+		road.style.top = value * 0.15 + 'px';
+	})
 </script>
 
-<h1>Hello {name}!</h1>
+<main class="font-main">
+	<section>
+		<img src="images/bg.jpg" id="bg" alt = "bg">
+		<img src="images/moon.png" id="moon" alt = "bg">
+		<img src="images/mountain.png" id="mountain" alt = "bg">
+		<img src="images/road.png" id="road" alt = "bg">
+		<h2 id="text">Moon Light</h2>
+	</section>
+</main>
+
+
+
+<style lang="postcss" global>
+	@import url('https//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap');
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+	
+
+	* {
+		margin: 0;
+		padding: 0;
+		font-family: 'Poppins', sans-serif;
+	}
+
+	main {
+		background: #0a2a43;
+		min-height: 1500px;
+	}
+
+	section {
+		position: relative;
+		width: 100%;
+		height: 100vh;
+		overflow: hidden;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	section:before {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+		height: 100px;
+		background: linear-gradient(to top, #0a2a43, transparent);
+		z-index: 10000;
+	}
+
+	section:after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: #0a2a43;
+		z-index: 10000;
+		mix-blend-mode: color;
+
+	}
+
+	section img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		pointer-events: none;
+	}
+
+	#text {
+		position: relative;
+		color: #fff;
+		font-size: 10em;
+		z-index: 1;
+	}
+</style>
